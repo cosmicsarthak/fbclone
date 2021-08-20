45ac281f271d92ae647b5d8847822a37e08b10fa
cosmicsarthak <sarthakmohanty200@gmail.com>
2021-08-20 11:42:35 +0530

    modified

---
+++ components/Header.js
@@ -0,0 +1,66 @@
+import Image from "next/image";
+import HeaderIcon from "./HeaderIcon";
+
+import {
+  BellIcon,
+  ChatIcon,
+  ChevronDownIcon,
+  HomeIcon,
+  UserGroupIcon,
+  ViewGridIcon,
+} from "@heroicons/react/solid";
+import {
+  FlagIcon,
+  PlayIcon,
+  SearchIcon,
+  ShoppingCartIcon,
+} from "@heroicons/react/outline";
+
+function Header() {
+  return (
+    <div className="sticky top-0 z-50 bg-white flex items-center p-2 lg:px-5 shadow-md">
+      {/* left */}
+      <div className="flex items-center">
+        <Image
+          src="https://links.papareact.com/5me" //https://cdn0.iconfinder.com/data/icons/social-messaging-ui-color-shapes-2-free/128/social-facebook-2019-circle-512.png
+          width={40}
+          height={40}
+          layout="fixed"
+        />
+
+        <div className="flex ml-2 items-center rounded-full bg-gray-100 p-2">
+          <SearchIcon className="h-6 text-gray-500" />
+          <input
+            className="hidden md:inline-flex ml-2 items-center bg-transparent outline-none placeholder-gray-500 flex-shrink"
+            type="text"
+            placeholder="Search Facebook"
+          />
+        </div>
+      </div>
+
+      {/* center */}
+      <div className="flex justify-center flex-grow">
+        <div className="flex space-x-6 md:space-x-2">
+          <HeaderIcon active Icon={HomeIcon} />
+          <HeaderIcon Icon={FlagIcon} />
+          <HeaderIcon Icon={PlayIcon} />
+          <HeaderIcon Icon={ShoppingCartIcon} />
+          <HeaderIcon Icon={UserGroupIcon} />
+        </div>
+      </div>
+
+      {/* right */}
+      <div className="flex items-center sm:space-x-2 justify-end">
+        {/* Profile Pic */}
+
+        <p className="whitespace-nowrap font-semibold pr-3">Sarthak Mohanty</p>
+        <ViewGridIcon className="icon" />
+        <ChatIcon className="icon" />
+        <BellIcon className="icon" />
+        <ChevronDownIcon className="icon" />
+      </div>
+    </div>
+  );
+}
+
+export default Header;
